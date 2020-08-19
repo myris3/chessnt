45bec69cc5b2980e646b5e9f0ab64bf722eb430b
Fritz-Olav Myrvang <fritzm@netcompany.com>
2020-08-19 10:03:22 +0200

    Updated the vue project, this is so far from finished

---
+++ src/components/ChessBoard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="tile">
+    <h1>{{ msg1 }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChessTile',
+  props: {
+    msg1: Boolean
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
---
+++ src/components/EmployeeForm.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "EmployeeForm"
+}
+</script>
+
+<style scoped>
+
+</style>
